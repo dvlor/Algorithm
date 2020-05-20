a7205e9e8a5382ca83378bf1bf6a95996d05b30b
dvlor <dvlopr@163.com>
2020-05-20 17:31:34 +0800

    s

--- js/QSortCanvas.js
+++ js/QSortCanvas.js
@@ -1,88 +1,112 @@
-function QSortCanavs(el) {
-    this.context = el.getContext('2d');
-    var width = el.offsetWidth,
-        height = el.offsetHeight,
-        pillarWidth = 8,
-        pillarSpace = 2,
-        exchanges = [],
-        datas = [],
-        currentStep = 0;
-
-    var startX = 0;
-
-    init(this.context);
-
-    var _this = this;
-
-    function init(context) {
-        context.fillStyle = "#f00";
+/**
+ * a tool class for data sort visual
+ * @param {HTMLCanvasElement} el canvas element
+ */
+function createQSortCanavs(el, data) {
+    if (! el instanceof HTMLCanvasElement) {
+        throw 'el must be instance of HTMLCanvasElement'
     }
 
-    this.setData = function (data) {
-        startX = (width - data.length * (pillarSpace + pillarWidth) + pillarSpace) / 2;
+    const pillarWidth = 8;
+    const pillarSpace = 2;
+    const dataScale = 4;
+
+
+    var canvasWidth = el.offsetWidth,
+        canvasHeight = el.offsetHeight,
+        exchanges = [],
+        dataLength = data.length,
+        startX = 0,
+        context = el.getContext('2d'),
+        currentStep = 0;
+
+    var qSortObjects = init(context, canvasWidth, canvasHeight, data, pillarSpace, pillarWidth);
+
+    /**
+     * show the next change animate
+     */
+    function next(callback) {
+        if (currentStep >= exchanges.length) {
+            callback && calback();
+            return
+        }
+        var needChange = exchanges[currentStep];
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+        qSortObjects.filter(data => needChange.indexOf(data.config.index) < 0).forEach(draw);
+        doAnimate(qSortObjects.filter(d => needChange.indexOf(d.config.index) > -1), function () {
+            currentStep ++;
+            callback && callback()
+        });
+    }
+
+    /**
+     * play data change
+     */
+    function play(callback) {
+        if (currentStep < exchanges.length) {
+            next(function () {
+                play(callback);
+            })
+        } else {
+            qSortObjects.forEach(draw);
+            callback && callback()
+        }
+    }
+
+    
+
+    function draw(obj) {
+        obj.draw(context, startX, canvasHeight, dataScale, pillarWidth, pillarSpace);
+    }
+
+    function init(context, canvasWidth, canvasHeight, data, pillarSpace, pillarWidth, ) {
+        // set style
+        context.fillStyle = "#f00";
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+        // init basic info
+
+        startX = (canvasWidth - dataLength * (pillarSpace + pillarWidth) + pillarSpace) / 2;
+        var objs = [];
         for (var i = 0; i < data.length; i++) {
-            datas.push(new QSortObject({
+            objs.push(new QSortObject({
                 value: data[i],
                 index: i
             }));
         }
-        datas.forEach(obj => draw(obj));
+        objs.forEach(draw);
+
+        return objs;
     }
 
-    this.exch = function (data, src, dst) {
+    function exch(data, src, dst) {
         exchanges.push([src, dst]);
         var tmp = data[dst];
         data[dst] = data[src];
         data[src] = tmp;
     }
 
-    this.next = function (callback) {
-        if (currentStep >= exchanges.length) {
-            callback && calback();
-        }
-        var indexs = exchanges[currentStep];
-        _this.context.clearRect(0, 0, 400, 200);
-        datas.forEach(data => {
-            if (indexs.indexOf(data.config.index) > -1) {
-                return;
-            }
-            draw(data);
-        });
-        this.doAnimate(datas.filter(d => indexs.indexOf(d.config.index) > -1), 200, function () {
-            currentStep++;
-            callback && callback()
-        });
-
-    }
-
-    this.doAnimate = function (changes, time, callback) {
-        _this.context.save();
+    function doAnimate(changes, callback) {
         var tmp = changes[0].config.index;
         changes[0].config.index = changes[1].config.index;
         changes[1].config.index = tmp;
+        context.save()
+        context.fillStyle = '#00f';
         changes.forEach(a => draw(a));
+        context.restore()
         setTimeout(() => {
             callback && callback()
         }, 500);
     }
 
-    function draw(obj) {
-        obj.draw(_this.context, startX, height, 4, 4, 2);
-    }
+    return {exch, play}
 
-    this.play = function (callback) {
-        if (currentStep < exchanges.length) {
-            var _this = this;
-            this.next(function () {
-                _this.play(callback);
-            })
-        } else {
-            console.log('done')
-            callback && callback()
-        }
-    }
+
 }
 
+/**
+ * data obj
+ * @param {*} config 
+ */
 function QSortObject(config) {
     this.config = Object.assign({
         value: 0,
